6edbfc4a7b36337621e17a801de867c3c6ed9034
Farmerok <91467185+Farmerok@users.noreply.github.com>
2024-05-01 20:12:56 +0300

    update code

---
+++ locales.js
@@ -0,0 +1,100 @@
+const locales = {
+    'en': {
+        /* for index.html */
+        'current_language': 'Сurrent language:',
+        'select_language': 'Select language:',
+        'homePage': 'Home',
+        'faqPage-text': 'FAQ',
+        'welcome': 'Welcome to the developer page!',
+        'partner-name': 'InsiderKeeps',
+        'partner-description': 'is your reliable partner in the world of software. We provide up-to-date programs and useful information for you.',
+        'download-info': 'Download our programs and explore the information that will help you successfully use our software.',
+        'questions-info': 'If you have any questions, refer to the FAQ section to find answers to frequently asked questions.',
+        'projects-info': 'Go to the Projects section to learn more about our current projects and opportunities.',
+        /* for faq.html */
+        'button_procejts': 'Project',
+        'welcome-faq': 'Welcome to the FAQ section!',
+        'description': 'Here you can find answers to frequently asked questions.',
+        
+        'question-1': 'How to download the "Telegram-Helper" project?',
+        'answer-1': 'You can download the "Telegram-Helper" project by going to the <a href="https://farmerok.github.io/project">Projects</a> tab.',
+        
+        'question-2': 'How to download the "Telegram-Helper" project?',
+        'answer-2': 'The "Telegram-MailReader" project allows you to view messages from your email directly through Telegram and see the contents of emails. You can download the archive <a href="https://farmerok.github.io/project">here</a>.',
+        
+        'expand-button-text': 'Expand',
+        'collapse-button-text': 'Collapse',
+        /* for project.html */
+        'projects-heading': 'Project List',
+        'description-button-text': 'Description',
+        'download-button-text': 'Download',
+        'project-description-1': 'Your personal bot-assistant and helper, with Telegram you can control your system.',
+        'project-description-2': 'The bot allows you to view messages from your email directly through Telegram. You can see the contents of emails.',
+        'project-description-3': 'With this program, you will be able to remotely control your PC using a Telegram bot. [This is an analog version of Telegram-Helper but with an interface.]',
+    },
+    'ru': {
+        /* for index.html */
+        'current_language': 'Текущий язык:',
+        'select_language': 'Выберите язык:',
+        'homePage': 'Главная',
+        'faqPage-text': 'ЧаВо',
+        'welcome': 'Добро пожаловать на страницу разработчика!',
+        'partner-name': 'InsiderKeeps',
+        'partner-description': 'это ваш надежный партнер в мире программного обеспечения. Мы предоставляем актуальные программы и полезную информацию для вас.',
+        'download-info': 'Скачайте наши программы и ознакомьтесь с информацией, которая поможет вам успешно использовать наше ПО.',
+        'questions-info': 'Если у вас возникли вопросы, обратитесь к разделу ЧаВо, чтобы найти ответы на часто задаваемые вопросы.',
+        'projects-info': 'Пройдите в раздел Проекты, чтобы узнать больше о наших актуальных проектах и возможностях.',
+        /* for faq.html */
+        'button_procejts': 'Проекты',
+        'welcome-faq': 'Добро пожаловать в раздел FAQ!',
+        'description': 'Здесь вы можете найти ответы на часто задаваемые вопросы.',
+        
+        'question-1': 'Как скачать проект "Telegram-Helper"?',
+        'answer-1': 'Вы можете скачать проект "Telegram-Helper", перейдя во вкладку <a href="https://farmerok.github.io/project">Проекты</a>.',
+        
+        'question-2': 'Как скачать проект "Telegram-MailReader"?',
+        'answer-2': 'Проект "Telegram-MailReader" позволяет вам просматривать сообщения с вашей электронной почты напрямую через Telegram и видеть содержимое писем. Вы можете скачать архив <a href="https://farmerok.github.io/project">здесь</a>.',
+        
+        'expand-button-text': 'Развернуть',
+        'collapse-button-text': 'Свернуть',
+        /* for project.html */
+        'projects-heading': 'Список проектов',
+        'download-button-text': 'Скачать',
+        'description-button-text': 'Описание',
+        'project-description-1': 'Ваш личный бот-ассистент и помощник, с помощью Telegram вы можете управлять своей системой.',
+        'project-description-2': 'Бот позволяет вам просматривать сообщения с вашей электронной почты напрямую через Telegram. Вы сможете видеть содержимое писем.',
+        'project-description-3': 'С помощью этой программы вы сможете управлять своим ПК удаленно с помощью Telegram бота. [Это аналоговая версия Telegram-Helper, но с интерфейсом.]',
+    },
+    'uk': {
+        /* for index.html */
+        'current_language': 'Поточна мова:',
+        'select_language': 'Виберіть мову:',
+        'homePage': 'Головна',
+        'faqPage-text': 'Інформація',
+        'welcome': 'Ласкаво просимо на сторінку розробника!',
+        'partner-name': 'InsiderKeeps',
+        'partner-description': 'це ваш надійний партнер у світі програмного забезпечення. Ми надаємо актуальні програми та корисну інформацію для вас.',
+        'download-info': 'Завантажте наші програми та ознайомтеся з інформацією, яка допоможе вам успішно використовувати наше ПЗ.',
+        'questions-info': 'Якщо у вас виникли питання, зверніться до розділу ЧаПи, щоб знайти відповіді на часто задавані питання.',
+        'projects-info': 'Перейдіть у розділ Проекти, щоб дізнатися більше про наші актуальні проекти та можливості.',
+        /* for faq.html */
+        'button_procejts': 'Проекти',
+        'welcome-faq': 'Ласкаво просимо в розділ FAQ!',
+        'description': 'Тут ви можете знайти відповіді на часто задавані питання.',
+        
+        'question-1': 'Як скачати "Telegram-Helper"?',
+        'answer-1': 'Ви можете скачати перейшовши до вкладки <a href="https://farmerok.github.io/project">Проекти</a>.',
+        'question-2': 'Як скачати проект "Telegram-MailReader"?',
+        'answer-2': 'Проект "Telegram-MailReader" дозволяє вам переглядати повідомлення з вашої електронної пошти безпосередньо через Telegram і бачити зміст листів. Ви можете завантажити архів <a href="https://farmerok.github.io/project">тут</a>.',
+    
+        'expand-button-text': 'Розгорнути',
+        'collapse-button-text': 'Згорнути',
+        /* for project.html */
+        'projects-heading': 'Список проектів',
+        'download-button-text': 'Завантажити',
+        'description-button-text': 'Опис',
+        'project-description-1': 'Ваш особистий бот-асистент і помічник, за допомогою Telegram ви можете керувати своєю системою.',
+        'project-description-2': 'Бот дозволяє вам переглядати повідомлення з вашої електронної пошти безпосередньо через Telegram. Ви зможете бачити вміст листів.',
+        'project-description-3': 'За допомогою цієї програми ви зможете керувати своїм ПК віддалено за допомогою Telegram бота. [Це аналогова версія Telegram-Helper, але з інтерфейсом.]'
+    }
+};
