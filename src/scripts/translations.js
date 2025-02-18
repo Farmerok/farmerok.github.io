02925428fe98079617fbce5cbfce182657d3a502
insiderkeeps <91467185+Farmerok@users.noreply.github.com>
2025-02-18 12:11:56 +0200

    Updated

--- src/scripts/translations.js
+++ src/scripts/translations.js
@@ -1,10 +1,8 @@
 const translations = {
     en: {
-        "nav-logo": "InsiderKeeps Dev",
         "nav-home": "Home",
         "nav-about": "About",
         "nav-projects": "Products",
-        "nav-contact": "Contact",
         "hero-title": "Welcome to IK Client",
         "hero-description": "Discover healthy software tools and applications that will make your life easier",
         "scroll-text": "Scroll down",
@@ -20,17 +18,11 @@ const translations = {
         "project1-description": "Your personal assistant that lets you control your PC from your phone from anywhere—manage sound, screen, files, watch in real-time, and more.",
         "project2-description": "A Telegram bot that lets you read your emails directly in Telegram. View email contents without switching apps.",
         "project3-description": "Simple and easy to set up and use Auto Clicker. Choose the application, set the time until it clicks, and START!",
-
-        "contact-title": "Contact Me",
-        "telegram-title": "Telegram",
-        "email-title": "Email",
     },
     uk: {
-        "nav-logo": "InsiderKeeps Dev",
         "nav-home": "Головна",
         "nav-about": "Про мене",
         "nav-projects": "Продукти",
-        "nav-contact": "Контакти",
         "hero-title": "Ласкаво просимо до IK Client",
         "hero-description": "Відкрийте для себе корисні програмні інструменти та додатки, які полегшать ваше життя",
         "scroll-text": "Прокрутіть вниз",
@@ -45,18 +37,12 @@ const translations = {
         "projects-title": "Мої Продукти",
         "project1-description": "Ваш особистий асистент, з яким ви можете керувати своїм ПК з телефону з будь-якої точки світу—управляйте звуком, екраном, файлами, дивіться в реальному часі тощо.",
         "project2-description": "Телеграм-бот, який дозволяє читати ваші електронні листи прямо в Telegram. Переглядайте вміст листів без перемикання між додатками.",
-        "project3-description": "Простий та зручний у налаштуванні Auto Clicker. Виберіть додаток, встановіть час до натискання та ПОЧНІТЬ!",
-
-        "contact-title": "Зв'яжіться зі мною",
-        "telegram-title": "Телеграм",
-        "email-title": "Електронна пошта",
+        "project3-description": "Простий та зручний у налаштуванні Авто-Клікер Виберіть додаток, встановіть час до натискання та ПОЧНІТЬ!",
     },
     ru: {
-        "nav-logo": "InsiderKeeps Dev",
         "nav-home": "Главная",
         "nav-about": "Обо мне",
         "nav-projects": "Продукты",
-        "nav-contact": "Контакты",
         "hero-title": "Добро пожаловать в IK Client",
         "hero-description": "Откройте для себя полезные программные инструменты и приложения, которые сделают вашу жизнь проще",
         "scroll-text": "Прокрутите вниз",
@@ -71,10 +57,6 @@ const translations = {
         "projects-title": "Мои Продукты",
         "project1-description": "Ваш личный ассистент, с которым вы можете с телефона управлять своим ПК откуда угодно—управлять звуком, экраном, файлами, смотреть в реальном времени и т. д.",
         "project2-description": "Телеграм-бот, который позволяет читать ваши электронные письма прямо в Telegram. Просматривайте содержимое писем без переключения между приложениями.",
-        "project3-description": "Простой и удобный в настройке Auto Clicker. Выберите приложение, установите время до клика и НАЧНИТЕ!",
-
-        "contact-title": "Свяжитесь со мной",
-        "telegram-title": "Телеграм",
-        "email-title": "Электронная почта",
+        "project3-description": "Простой и удобный в настройке Авто-Кликер. Выберите приложение, установите время до клика и НАЧНИТЕ!",
     }
 };
